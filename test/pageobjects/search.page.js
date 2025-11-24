39c3d7eec7f3f4bf5d92ca7c5ec55bd586b0555c
umabindhuqa <umabindhupoatla@gmail.com>
2025-11-24 20:15:09 +0530

    Update search.page.js

--- test/pageobjects/search.page.js
+++ test/pageobjects/search.page.js
@@ -1,1 +1,11 @@
-class SearchPage{}; module.exports=new SearchPage();
+class SearchPage {
+    get searchInput() { return $('#search'); }
+    get results() { return $$('#results .item'); }
+
+    async search(term) {
+        await this.searchInput.setValue(term);
+        await browser.keys('Enter');
+    }
+}
+
+module.exports = new SearchPage();
