45a216814a61b1af8ede67d35ff0130600419a4b
umabindhuqa <umabindhupoatla@gmail.com>
2025-11-24 20:14:37 +0530

    Update login.page.js

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,1 +1,27 @@
-class LoginPage{}; module.exports=new LoginPage();
+class LoginPage {
+    /**
+     * Define selectors using getter methods
+     */
+    get inputUsername() {
+        return $('#username'); 
+    }
+
+    get inputPassword() {
+        return $('#password'); 
+    }
+
+    get btnLogin() {
+        return $('#login'); 
+    }
+
+    /**
+     * A reusable login function
+     */
+    async login(username, password) {
+        await this.inputUsername.setValue(username);
+        await this.inputPassword.setValue(password);
+        await this.btnLogin.click();
+    }
+}
+
+module.exports = new LoginPage();
