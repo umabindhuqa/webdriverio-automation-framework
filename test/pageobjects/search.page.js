6a321b2c3c6c0840cd2bcf29e8bd85f11bf7f46b
umabindhuqa <umabindhupoatla@gmail.com>
2025-11-24 20:21:46 +0530

    Update search.page.js

--- test/pageobjects/search.page.js
+++ test/pageobjects/search.page.js
@@ -1,10 +1,56 @@
 class SearchPage {
-    get searchInput() { return $('#search'); }
-    get results() { return $$('#results .item'); }
 
-    async search(term) {
-        await this.searchInput.setValue(term);
-        await browser.keys('Enter');
+    // ============================
+    // Selectors
+    // ============================
+    get searchInput() {
+        return $('#search');
+    }
+
+    get searchButton() {
+        return $('#searchBtn');
+    }
+
+    get searchResults() {
+        return $$('.result-item');
+    }
+
+    get noResultsMessage() {
+        return $('#noResults');
+    }
+
+    // ============================
+    // Actions
+    // ============================
+
+    /**
+     * Open Search Page
+     */
+    open() {
+        browser.url('/search');
+    }
+
+    /**
+     * Perform a search
+     * @param {string} keyword 
+     */
+    async search(keyword) {
+        await this.searchInput.setValue(keyword);
+        await this.searchButton.click();
+    }
+
+    /**
+     * Get number of results displayed
+     */
+    async getResultsCount() {
+        return await this.searchResults.length;
+    }
+
+    /**
+     * Get "No results found" text
+     */
+    async getNoResultsText() {
+        return await this.noResultsMessage.getText();
     }
 }
 
