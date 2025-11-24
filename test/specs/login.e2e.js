22643d9a20121c1659c602e0e5372139fdf7aa40
umabindhuqa <umabindhupoatla@gmail.com>
2025-11-24 20:15:32 +0530

    Update login.e2e.js

--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -1,1 +1,9 @@
-describe('login',()=>{});
+const LoginPage = require('../pageobjects/login.page');
+
+describe('Login Feature', () => {
+    it('should login successfully', async () => {
+        await browser.url('https://example.com');
+        await LoginPage.login('admin', 'password');
+        await expect(browser).toHaveUrlContaining('dashboard');
+    });
+});
