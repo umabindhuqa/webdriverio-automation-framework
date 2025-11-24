6a39cb82fe9ddda815f8f275ded8b570b91182e0
umabindhuqa <umabindhupoatla@gmail.com>
2025-11-24 20:17:03 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,1 +1,19 @@
-exports.config={runner:'local',specs:['./test/specs/**/*.js'],capabilities:[{browserName:'chrome'}],framework:'mocha'};
+exports.config = {
+    runner: 'local',
+    specs: ['./test/specs/**/*.js'],
+
+    maxInstances: 1,
+    capabilities: [{
+        browserName: 'chrome'
+    }],
+
+    logLevel: 'error',
+
+    framework: 'mocha',
+    reporters: ['spec'],
+
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    }
+}
