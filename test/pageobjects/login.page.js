2c57cbd37b81fe8792d7b9e2f41c89fa67fd7c76
umabindhuqa <umabindhupoatla@gmail.com>
2025-11-24 20:20:37 +0530

    Update login.page.js

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,26 +1,51 @@
 class LoginPage {
+
+    // ============================
+    // Selectors
+    // ============================
+    get usernameInput() {
+        return $('#username');
+    }
+
+    get passwordInput() {
+        return $('#password');
+    }
+
+    get loginBtn() {
+        return $('#loginBtn');
+    }
+
+    get errorMessage() {
+        return $('#error');
+    }
+
+    // ============================
+    // Actions
+    // ============================
+
     /**
-     * Define selectors using getter methods
+     * Open Login Page
      */
-    get inputUsername() {
-        return $('#username'); 
-    }
-
-    get inputPassword() {
-        return $('#password'); 
-    }
-
-    get btnLogin() {
-        return $('#login'); 
+    open() {
+        browser.url('/login');
     }
 
     /**
-     * A reusable login function
+     * Login using username & password
+     * @param {string} username 
+     * @param {string} password 
      */
     async login(username, password) {
-        await this.inputUsername.setValue(username);
-        await this.inputPassword.setValue(password);
-        await this.btnLogin.click();
+        await this.usernameInput.setValue(username);
+        await this.passwordInput.setValue(password);
+        await this.loginBtn.click();
+    }
+
+    /**
+     * Get error text (if login fails)
+     */
+    async getErrorText() {
+        return await this.errorMessage.getText();
     }
 }
 
