26bb9a06a104d3cfc23264bea675879238a6fa25
umabindhuqa <umabindhupoatla@gmail.com>
2025-11-24 20:25:40 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,19 +1,63 @@
 exports.config = {
-    runner: 'local',
-    specs: ['./test/specs/**/*.js'],
 
+    //
+    // ====================
+    // Runner Configuration
+    // ====================
+    runner: 'local',
+
+    //
+    // ====================
+    // Test Files
+    // ====================
+    specs: [
+        './test/specs/**/*.js'
+    ],
+
+    //
+    // ============
+    // Capabilities
+    // ============
     maxInstances: 1,
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
+        acceptInsecureCerts: true
     }],
 
-    logLevel: 'error',
+    //
+    // ===================
+    // Test Configurations
+    // ===================
+    logLevel: 'info',
+
+    bail: 0,
+    baseUrl: 'https://example.com',   // Change to your app url later
+
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
 
     framework: 'mocha',
+    
     reporters: ['spec'],
 
     mochaOpts: {
         ui: 'bdd',
         timeout: 60000
-    }
-}
+    },
+
+    //
+    // =================
+    // Hooks
+    // =================
+
+    /**
+     * Take screenshot automatically on failure
+     */
+    afterTest: async function (test, context, { error }) {
+        if (error) {
+            await browser.saveScreenshot(`./errorShots/${test.title}.png`);
+        }
+    },
+
+};
